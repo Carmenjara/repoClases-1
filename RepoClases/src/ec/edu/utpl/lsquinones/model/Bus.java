70dc5254395c8e53fee49c554841d0589f54657a
Carmenjara <chiojaramillo2@gmail.com>
2016-07-12 11:00:08 -0500

    Update Bus.java

--- RepoClases/src/ec/edu/utpl/lsquinones/model/Bus.java
+++ RepoClases/src/ec/edu/utpl/lsquinones/model/Bus.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package ec.edu.utpl.lsquinones.model;
 
 /**
  *
- * @author Santiago Quiñones
+ * @author Rocio Jaramillo
  */
 public class Bus {
 
